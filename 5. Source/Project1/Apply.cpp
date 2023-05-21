03c282668daec24c928277c8a306d17addbd899f
pupukii <wj3507@naver.com>
2023-05-21 11:01:27 +0900

    apply

---
+++ 5. Source/Project1/Apply.cpp
@@ -0,0 +1,16 @@
+#include "Apply.h"
+
+Apply::Apply(string Companyname, int businessNumber, string taskType, int headCount, string Deadline) {
+	this->Companyname = Companyname; 
+	this->businessNumber = businessNumber;
+	this->taskType = taskType;
+	this->headCount = headCount;
+	this->Deadline = Deadline;
+}
+
+void Apply::getDetail() {
+	
+}
+NormalMember getNormalMember(){
+	
+}
